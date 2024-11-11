c9de0946f04086841408ecdf59da57c9eddb03e1
RobsonJun10r <robson.alves.junior2@gmail.com>
2024-11-11 13:58:00 -0300

    Inicio seção 2

---
+++ Seção 2/Estruturas de progamação/js/script.js
@@ -0,0 +1,31 @@
+// Variáveis
+let nome = prompt("Qual o seu nome?");
+
+if (nome === "Robson") {
+  console.log("Esse cara é muito lindo");
+} else {
+  console.log("Esse cara é feio");
+}
+
+nome = "Robson Ribeiro Alves Junior";
+
+const idade = 21;
+console.log(idade)
+
+// Const é a palavra chave ideal para criar variável
+
+// 2 -Mais sobre variáveis
+
+let a = 5, b = 4, c = 10 //Criação de variáveis em sequência
+
+const nomecompleto = "Robson Ribeiro"
+const nomeCompleto = "Robson Ribeiro Alves"
+console.log(nomecompleto)
+console.log(nomeCompleto)
+
+//Formas permitidas de criar variáveis
+
+let _teste = "Ok"
+let $teste = "Ok"
+
+console.log(_teste, $teste)
